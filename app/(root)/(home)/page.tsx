db35c1055fc44daccc7bac1f2645691031581c15
Ar Koushal <arkoushal1999@gmail.com>
2023-09-23 12:51:52 -0400

    Initial commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl">
+      abc
+    </main>
+  );
+};
+
+export default Page;
