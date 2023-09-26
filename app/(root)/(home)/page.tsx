41fce0aaa259e690896f299b394786630f9ffa09
Ar Koushal <arkoushal1999@gmail.com>
2023-09-26 16:05:10 -0400

    implement search and styling

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,17 @@
+import Searchform from "@/components/Searchform";
 import React from "react";
 
 const Page = () => {
   return (
-    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl">
-      abc
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-coverbg-center text-center">
+          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
+            JavaScript Mastery Resources
+          </h1>
+        </div>
+        <Searchform />
+      </section>
     </main>
   );
 };
---
+++ components/Searchform.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+const Searchform = () => {
+  return (
+    <form className="flex-center mx-auto mt-10 w-full sm:-mt-6 sm:px-5">
+      <label className="flex-center relative w-full max-w-3xl">
+        <Image
+          src="/magnifying-glass.svg"
+          className="absolute left-8"
+          width={32}
+          height={32}
+          alt="search icon"
+        />
+        <Input
+          className=" base-regular h-fit border-0 bg-black-400 py-3 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800 "
+          type="text"
+          placeholder="Search"
+        />
+      </label>
+    </form>
+  );
+};
+
+export default Searchform;
